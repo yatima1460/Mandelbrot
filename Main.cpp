edd237420c8cd921416aebcbeb839a5226aee864
Yatima <yatima1460@pop-os.localdomain>
2020-07-23 01:17:20 +0200

    Distance

--- Main.cpp
+++ Main.cpp
@@ -5,21 +5,23 @@
 static const int WINDOW_WIDTH = 1280;
 static const int WINDOW_HEIGHT = 720;
 
-
-
 SDL_Event event;
 SDL_Renderer *renderer;
 SDL_Window *window;
 
-void DrawPoint(int x, int y) 
-{
-    SDL_RenderDrawPoint(renderer, WINDOW_WIDTH*0.5+x, WINDOW_HEIGHT*0.5+y);
+float Distance(const int x0, const int y0, const int x1, const int y1) {
+  return sqrtf((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0));
+}
+
+void DrawPoint(const int x, const int y) {
+  SDL_RenderDrawPoint(renderer, WINDOW_WIDTH * 0.5 + x,
+                      WINDOW_HEIGHT * 0.5 + y);
 }
 
 void InitVideo() {
   SDL_Init(SDL_INIT_VIDEO);
-  SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL, &window,
-                              &renderer);
+  SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL,
+                              &window, &renderer);
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
   SDL_RenderClear(renderer);
   SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
@@ -27,8 +29,7 @@ void InitVideo() {
 
 void DrawMandelbrot() {
   for (int i = 0; i < WINDOW_WIDTH; ++i)
-    DrawPoint(i,i);
-    
+    DrawPoint(i, i);
 
   SDL_RenderPresent(renderer);
 }
