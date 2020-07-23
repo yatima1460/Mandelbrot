e6cd55a5e14b18427ec8bce683a150a4467af7a7
Yatima <yatima1460@pop-os.localdomain>
2020-07-23 05:55:04 +0200

    colors

--- Main.c
+++ Main.c
@@ -7,17 +7,17 @@ static const int WINDOW_PIXELS_SIDE_LENGTH = 1000;
 
 // More iterations = slower but more sharp edges
 // Less iterations = faster but blobby Mandelbrot
-static const int ITERATIONS_PER_COMPLEX_NUMBER = 50;
+static const int ITERATIONS_PER_COMPLEX_NUMBER = 30;
 
 // <1.0 = you risk to generate an image with holes (super fast)
 // 1.0 = 1 complex point tested / pixel (normal nice image)
 // >1.0 = more complex points tested / pixel (super-resolution SLOWER)
-static const float PRECISION = 4.0F;
+static const float DENSITY = 16.0F;
 
 // The zoom level
 // Mandelbrot doesn't exist above distance 2 with 0,0 so it's pointless so go above
 static const float LIMIT = 2.0F;
-static const float STEP = ((LIMIT * 2.0F) / WINDOW_PIXELS_SIDE_LENGTH) / PRECISION;
+static const float STEP = ((LIMIT * 2.0F) / WINDOW_PIXELS_SIDE_LENGTH) / DENSITY;
 
 SDL_Event event;
 SDL_Renderer* renderer;
@@ -31,7 +31,9 @@ void InitVideo() {
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_CreateWindowAndRenderer(
 		WINDOW_PIXELS_SIDE_LENGTH, WINDOW_PIXELS_SIDE_LENGTH, SDL_WINDOW_OPENGL, &window, &renderer);
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+	//SDL_SetRenderDrawColor(renderer, 18, 40, 99, 255);
 	SDL_RenderClear(renderer);
 	// SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 }
@@ -47,35 +49,46 @@ float* CalculateMandelbrot() {
 		fprintf(stderr, "Not enough memory!");
 		abort();
 	}
-for (int px = 0; px < WINDOW_PIXELS_SIDE_LENGTH; px++) {
-	for (int py = 0; py < WINDOW_PIXELS_SIDE_LENGTH; py++) {
+
+	for (int i = 0; i < WINDOW_PIXELS_SIDE_LENGTH * WINDOW_PIXELS_SIDE_LENGTH; i++) {
+		const int px = i % WINDOW_PIXELS_SIDE_LENGTH;
+		const int py = i / WINDOW_PIXELS_SIDE_LENGTH;
+
+		const float x = map(px, 0, WINDOW_PIXELS_SIDE_LENGTH, -LIMIT, LIMIT);
+		const float y = map(py, 0, WINDOW_PIXELS_SIDE_LENGTH, -LIMIT, LIMIT);
+		n = 0 + 0 * I;
+		c = x + y * I;
+
 		
+		for (int j = 0;  j < ITERATIONS_PER_COMPLEX_NUMBER; j++)
+		{
+			// Multiply c = zoom level
+			// Add complex = offset
+			n = n * n + c;
 
-			const float x = map(px, 0, WINDOW_PIXELS_SIDE_LENGTH,-LIMIT, LIMIT );
-			const float y = map(py, 0, WINDOW_PIXELS_SIDE_LENGTH,-LIMIT, LIMIT );
-			n = 0 + 0 * I;
-			c = x + y * I;
+			const float n_real = creal(n);
+			const float n_imag = cimag(n);
 
-			int i = 0;
-			while (i < ITERATIONS_PER_COMPLEX_NUMBER) {
-				// Multiply c = zoom level
-				// Add complex = offset
-				n = n * n + c;
+			*(stability + py * WINDOW_PIXELS_SIDE_LENGTH + px) = 0;
+			// If the complex number distance with the origin is > 2 than it's unstable
+			if (n_real * n_real + n_imag * n_imag > 4.0) {
 
-				const float n_real = creal(n);
-				const float n_imag = cimag(n);
-
-				// If the complex number distance with the origin is > 2 than it's unstable
-				if (n_real * n_real + n_imag * n_imag > 4.0) {
-					
-					// Stability color is based on number of iterations reached
-					const float color = ((float)(i) / (float)(ITERATIONS_PER_COMPLEX_NUMBER));
+				
+					const float color = ((float)(j) / (float)(ITERATIONS_PER_COMPLEX_NUMBER));
 					*(stability + py * WINDOW_PIXELS_SIDE_LENGTH + px) = color;
-					break;
-				}
-				i++;
+				
+				
+				break;
+				
+				// Stability color is based on number of iterations reached
+				
 			}
 		}
+		
+		
+			
+		
+		
 	}
 
 	return stability;
@@ -83,11 +96,15 @@ for (int px = 0; px < WINDOW_PIXELS_SIDE_LENGTH; px++) {
 
 void DrawMandelbrot(const float* const stabilityPoints) {
 	assert(stabilityPoints != NULL);
+	
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 	for (int i = 0; i < WINDOW_PIXELS_SIDE_LENGTH * WINDOW_PIXELS_SIDE_LENGTH; i++) {
 		const int x = i % WINDOW_PIXELS_SIDE_LENGTH;
 		const int y = i / WINDOW_PIXELS_SIDE_LENGTH;
 		const float color = stabilityPoints[i] * 255;
-		SDL_SetRenderDrawColor(renderer, color, color, color, 255);
+	
+		SDL_SetRenderDrawColor(renderer, color, color, 255-color, 255*stabilityPoints[i]);
+	
 		SDL_RenderDrawPoint(renderer, x, y);
 	}
 	// Draw image on screen
